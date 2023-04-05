7f4cfdbb8be97de04d979c57efabba5850e94a97
Andy <andy-klinkers@hotmail.com>
2023-04-05 23:13:30 +0200

    More setup

--- BackEnd/DataAccessLayer/Configurations/UserConfiguration.cs
+++ BackEnd/DataAccessLayer/Configurations/UserConfiguration.cs
@@ -13,18 +13,20 @@ namespace DataAccessLayer.Configurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            builder.HasKey(x => x.Id);
+
             builder.Property(x => x.UserName)
-                .HasMaxLength(30)
+                .HasMaxLength(255)
                 .IsRequired();
 
             builder.Property(x => x.FirstName)
-                .HasMaxLength(30);
+                .HasMaxLength(255);
 
             builder.Property(x => x.LastName)
-                .HasMaxLength(30);
+                .HasMaxLength(255);
 
             builder.Property(x => x.Email)
-                .HasMaxLength(30)
+                .HasMaxLength(255)
                 .IsRequired();
 
             builder.Property(x => x.Password)
